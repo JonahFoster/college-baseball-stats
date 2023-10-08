1f8a4be4ca9ad51ba943db88f11e3f346d75dd91
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-08 14:47:35 -0400

    Initial Frontend Commit

---
+++ college-baseball-app/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className="input-search">
+      <input type="text" className="input-search-bar" required />
+      <label className="input-search-label">Search</label>
+    </div>
+  )
+}
