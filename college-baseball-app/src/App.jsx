76208b83a9b5559de0d9b4b7e54c58d838c72ada
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-09 09:43:34 -0400

    MaterialUI components added

--- college-baseball-app/src/App.jsx
+++ college-baseball-app/src/App.jsx
@@ -1,19 +1,36 @@
+import { useRef } from 'react';
+import { TextField, Button, Grid } from '@mui/material';
 import './App.css';
 
 export default function App() {
+  const inputRef = useRef();
 
   function handleSearch() {
-    const name = document.querySelector('.input-search-bar').value;
+    const name = inputRef.current.value;
     window.location.href = `http://localhost:3000/${name.replace(/\s/g, '-')}`;
   }
 
   return (
-    <div className="input-search">
-      <input 
-        type="text" 
-        className="input-search-bar"
-      />
-      <button onClick={handleSearch}>Search</button>
-    </div>
-  );
+    <Grid 
+      container 
+      direction="row" 
+      alignItems="center"
+      justifyContent="center" 
+      style={{ minHeight: '20vh' }}
+    >
+      <Grid item>
+        <TextField 
+          id="outlined-basic" 
+          label="Search" 
+          variant="outlined" 
+          inputRef={inputRef}
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" onClick={handleSearch} sx={{ marginLeft: 2 }}>
+          Search
+        </Button>
+      </Grid>
+    </Grid>
+    );
 }
