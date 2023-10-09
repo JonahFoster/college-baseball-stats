5127eab16bcc9edfdd2be61928a4723888c7ab4e
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-09 09:07:56 -0400

    Frontend and Backend connected together

--- college-baseball-app/src/App.jsx
+++ college-baseball-app/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
 
 export default function App() {
 
+  function handleSearch() {
+    const name = document.querySelector('.input-search-bar').value;
+    window.location.href = `http://localhost:3000/${name.replace(/\s/g, '-')}`;
+  }
+
   return (
     <div className="input-search">
-      <input type="text" className="input-search-bar" required />
-      <label className="input-search-label">Search</label>
+      <input 
+        type="text" 
+        className="input-search-bar"
+      />
+      <button onClick={handleSearch}>Search</button>
     </div>
-  )
+  );
 }
