1bd530af0e5b5bcd439d96428ea6ce7eeed77ec1
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-12 19:22:48 -0400

    Finished Batting and Pitching components

---
+++ college-baseball-app/src/Fielding.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types';
+import { TableContainer, Paper, Table, TableBody, TableCell, TableHead, TableRow, Container } from '@mui/material';
+
+export default function Fielding({ data }) {
+  const stats = data[0];
+
+  return (
+    <Container>
+        <h1>{data[0].name}</h1>
+        <TableContainer component={Paper} style={{margin: 'auto' }}>
+            <h2>Batting</h2>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Season</TableCell>
+                        <TableCell>GP</TableCell>
+                        <TableCell>GS</TableCell>
+                        <TableCell>R</TableCell>
+                        <TableCell>AB</TableCell>
+                        <TableCell>H</TableCell>
+                        <TableCell>2B</TableCell>
+                        <TableCell>3B</TableCell>
+                        <TableCell>TB</TableCell>
+                        <TableCell>HR</TableCell>
+                        <TableCell>RBI</TableCell>
+                        <TableCell>BB</TableCell>
+                        <TableCell>HBP</TableCell>
+                        <TableCell>SF</TableCell>
+                        <TableCell>SH</TableCell>
+                        <TableCell>K</TableCell>
+                        <TableCell>CS</TableCell>
+                        <TableCell>PICKED</TableCell>
+                        <TableCell>SB</TableCell>
+                        <TableCell>IBB</TableCell>
+                        <TableCell>GDP</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>{stats.season}</TableCell>
+                        <TableCell>{stats.GP}</TableCell>
+                        <TableCell>{stats.GS}</TableCell>
+                        <TableCell>{stats.R}</TableCell>
+                        <TableCell>{stats.AB}</TableCell>
+                        <TableCell>{stats.H}</TableCell>
+                        <TableCell>{stats['2B']}</TableCell>
+                        <TableCell>{stats['3B']}</TableCell>
+                        <TableCell>{stats.TB}</TableCell>
+                        <TableCell>{stats.HR}</TableCell>
+                        <TableCell>{stats.RBI}</TableCell>
+                        <TableCell>{stats.BB}</TableCell>
+                        <TableCell>{stats.HBP}</TableCell>
+                        <TableCell>{stats.SF}</TableCell>
+                        <TableCell>{stats.SH}</TableCell>
+                        <TableCell>{stats.K}</TableCell>
+                        <TableCell>{stats.CS}</TableCell>
+                        <TableCell>{stats.Picked}</TableCell>
+                        <TableCell>{stats.SB}</TableCell>
+                        <TableCell>{stats.IBB}</TableCell>
+                        <TableCell>{stats.GDP}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Container>
+  );
+}
+
+Fielding.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
---
+++ college-baseball-app/src/Pitching.jsx
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types';
+import { TableContainer, Paper, Table, TableBody, TableCell, TableHead, TableRow, Container } from '@mui/material';
+
+export default function Pitching({ data }) {
+  const stats = data[0];
+
+  return (
+    <Container>
+        <h1>{stats.name}</h1>
+        <TableContainer component={Paper} style={{margin: 'auto' }}>
+            <h2>Batting</h2>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Season</TableCell>
+                        <TableCell>APP</TableCell>
+                        <TableCell>R</TableCell>
+                        <TableCell>H</TableCell>
+                        <TableCell>2B-A</TableCell>
+                        <TableCell>3B-A</TableCell>
+                        <TableCell>HR-A</TableCell>
+                        <TableCell>RBI</TableCell>
+                        <TableCell>BB</TableCell>
+                        <TableCell>ERA</TableCell>
+                        <TableCell>IP</TableCell>
+                        <TableCell>ER</TableCell>
+                        <TableCell>SO</TableCell>
+                        <TableCell>BF</TableCell>
+                        <TableCell>P-OAB</TableCell>
+                        <TableCell>WP</TableCell>
+                        <TableCell>HB</TableCell>
+                        <TableCell>SHA</TableCell>
+                        <TableCell>SFA</TableCell>
+                        <TableCell>Pitches</TableCell>
+                        <TableCell>GO</TableCell>
+                        <TableCell>FO</TableCell>
+                        <TableCell>W</TableCell>
+                        <TableCell>L</TableCell>
+                        <TableCell>KL</TableCell>
+                        <TableCell>Pickoffs</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>{stats.season}</TableCell>
+                        <TableCell>{stats.App}</TableCell>
+                        <TableCell>{stats.R}</TableCell>
+                        <TableCell>{stats.H}</TableCell>
+                        <TableCell>{stats['2B-A']}</TableCell>
+                        <TableCell>{stats['3B-A']}</TableCell>
+                        <TableCell>{stats['HR-A']}</TableCell>
+                        <TableCell>{stats.RBI}</TableCell>
+                        <TableCell>{stats.BB}</TableCell>
+                        <TableCell>{stats.ERA}</TableCell>
+                        <TableCell>{stats.IP}</TableCell>
+                        <TableCell>{stats.ER}</TableCell>
+                        <TableCell>{stats.SO}</TableCell>
+                        <TableCell>{stats.BF}</TableCell>
+                        <TableCell>{stats['P-OAB']}</TableCell>
+                        <TableCell>{stats.WP}</TableCell>
+                        <TableCell>{stats.HB}</TableCell>
+                        <TableCell>{stats.SHA}</TableCell>
+                        <TableCell>{stats.SFA}</TableCell>
+                        <TableCell>{stats.pitches}</TableCell>
+                        <TableCell>{stats.GO}</TableCell>
+                        <TableCell>{stats.FO}</TableCell>
+                        <TableCell>{stats.W}</TableCell>
+                        <TableCell>{stats.L}</TableCell>
+                        <TableCell>{stats.KL}</TableCell>
+                        <TableCell>{stats.pickoffs}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Container>
+  );
+}
+
+Pitching.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
