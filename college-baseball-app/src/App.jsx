35dd60ade9a91400260d808b262d913ba1e78c27
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-27 12:40:22 -0400

    TopList formatting

--- college-baseball-app/src/App.jsx
+++ college-baseball-app/src/App.jsx
@@ -107,30 +107,30 @@ export default function App() {
         <Container>
           <Grid container justifyContent="space-between" mt={2}>
             <Grid item xs={2.5}>
-              <TopList data={topListData.hr} title="Top HR"/>
+              <TopList data={topListData.hr} title="Top HR" statKey="HR"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.ba} title="Top BA"/>
+            <TopList data={topListData.ba} title="Top BA" statKey="batting_average"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.ops} title="Top OPS"/>
+            <TopList data={topListData.ops} title="Top OPS" statKey="OPS"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.bb} title="Top BB"/>
+            <TopList data={topListData.bb} title="Top BB" statKey="walks"/>
             </Grid>  
           </Grid>
           <Grid container justifyContent="space-between" mt={2}>
             <Grid item xs={2.5}>
-              <TopList data={topListData.era} title="Top ERA"/>
+              <TopList data={topListData.era} title="Top ERA" statKey="ERA"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.k} title="Top K"/>
+            <TopList data={topListData.k} title="Top K" statKey="strikeouts"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.kp} title="Top K%"/>
+            <TopList data={topListData.kp} title="Top K%" statKey="strikeout_percentage"/>
             </Grid>
             <Grid item xs={2.5}>
-            <TopList data={topListData.baa} title="Top BAA"/>
+            <TopList data={topListData.baa} title="Top BAA" statKey="batting_average_against"/>
             </Grid>  
           </Grid>
         </Container>
--- college-baseball-app/src/TopList.jsx
+++ college-baseball-app/src/TopList.jsx
@@ -1,22 +1,23 @@
-import { Card, List, ListItemText, Typography } from '@mui/material'
+import { Card, List, ListItem, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
-export default function TopList({ data, title}) {
+export default function TopList({ data, title, statKey }) {
     return (
         <Card variant="outlined">
             <Typography variant="h6">{title}</Typography>
             <List>
                 {data.map((item, index) => (
-                <ListItemText key={index}>
-                    <Typography>{item.name}</Typography>
-                </ListItemText>
+                    <ListItem key={index}>
+                        <Typography>{`${index + 1}. ${item.name} - ${item[statKey]}`}</Typography>
+                    </ListItem>
                 ))}
             </List>
         </Card>
-    )
+    );
 }
 
 TopList.propTypes = {
     data: PropTypes.arrayOf(PropTypes.object).isRequired,
     title: PropTypes.string.isRequired,
-  };
+    statKey: PropTypes.string.isRequired,
+};
