4b9a4b6bd633fc25ead6a1edfcf73e37d8798845
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-10-28 13:52:08 -0400

    Integration with school logo and colors

--- college-baseball-app/src/Player.jsx
+++ college-baseball-app/src/Player.jsx
@@ -1,37 +1,71 @@
-import PropTypes from 'prop-types'
-import { Container, Card, CardContent, Typography, Box, Chip } from '@mui/material'
-import Batting from './Batting'
-import Fielding from './Fielding'
-import Pitching from './Pitching'
+import PropTypes from 'prop-types';
+import { Container, Card, CardContent, Typography, Box, Chip } from '@mui/material';
+import Batting from './Batting';
+import Fielding from './Fielding';
+import Pitching from './Pitching';
+import teamInfo from './assets/logos.json';
+import './assets/player.css'
 
 export default function Player({ data }) {
-  const playerInfo = data[0]
-  const { name, Jersey, pos, Yr, school_name } = playerInfo
+  const playerInfo = data[0];
+  const { name, Jersey, pos, Yr, school_name } = playerInfo;
+
+  const teamData = teamInfo.find(team => team.ncaa_name === school_name);
 
   const hasBatting = !!data.find(stat => stat.data_type === 'batting' && stat.AB !== 0);
-  const hasFielding = !!data.find(stat => stat.data_type === 'fielding')
-  const hasPitching = !!data.find(stat => stat.data_type === 'pitching')
+  const hasFielding = !!data.find(stat => stat.data_type === 'fielding');
+  const hasPitching = !!data.find(stat => stat.data_type === 'pitching');
 
   function createFullYear(year) {
-    if (year === "Fr") return "Freshman"
-    if (year === "So") return "Sophomore"
-    if (year === "Jr") return "Junior"
-    if (year === "Sr") return "Senior"
+    if (year === "Fr") return "Freshman";
+    if (year === "So") return "Sophomore";
+    if (year === "Jr") return "Junior";
+    if (year === "Sr") return "Senior";
   }
 
   return (
     <Container>
       <Card variant="outlined">
-        <CardContent>
-          <Typography variant="h4">
-            {name}
-          </Typography>
-          <Box mt={2}>
-            <Chip label={school_name} color="primary" sx={{ mr: '5px' }}/>
-            <Chip label={`#${Jersey}`} color="primary" sx={{ mr: '5px' }}/>
-            <Chip label={pos} color="primary" sx={{ mr: '5px' }}/>
-            <Chip label={createFullYear(Yr)} color="primary"/>
+        <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Box>
+            <Typography variant="h4">
+              {name}
+            </Typography>
+            <Box mt={2}>
+            <Chip 
+              label={school_name} 
+              sx={{ 
+                mr: '5px',
+                backgroundColor: teamData.primary,
+                color: theme => theme.palette.getContrastText(teamData.primary),
+              }}
+            />
+            <Chip 
+              label={`#${Jersey}`} 
+              sx={{ 
+                mr: '5px',
+                backgroundColor: teamData.primary,
+                color: theme => theme.palette.getContrastText(teamData.primary),
+              }}
+            />
+            <Chip 
+              label={pos} 
+              sx={{ 
+                mr: '5px',
+                backgroundColor: teamData.primary,
+                color: theme => theme.palette.getContrastText(teamData.primary),
+              }}
+            />
+            <Chip 
+              label={createFullYear(Yr)} 
+              sx={{ 
+                backgroundColor: teamData.primary,
+                color: theme => theme.palette.getContrastText(teamData.primary),
+              }}
+            />
+            </Box>
           </Box>
+          <img src={teamData.logos} alt={`${school_name} logo`} className="school-logo" />
         </CardContent>
       </Card>
       <Card variant="outlined" sx={{ mt: '10px' }}>
@@ -40,9 +74,9 @@ export default function Player({ data }) {
         {hasFielding && <Fielding data={data} />}
       </Card>
     </Container>
-  )
+  );
 }
 
 Player.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+};
