1833b6a06347aa2448c2c6a25383cc5e297ebc37
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-11-05 08:17:26 -0500

    Truncate names that are too long on top ten lists

--- college-baseball-app/src/TopList.jsx
+++ college-baseball-app/src/TopList.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 export default function TopList({ data, title, statKey }) {
 
+    function truncatePlayerName(playerName) {
+        let maxLength = 17;
+        if (playerName.length > maxLength) {
+          return `${playerName.substring(0, maxLength - 3)}...`; // Subtract 3 for the ellipsis
+        }
+        return playerName;
+      }
+
   return (
     <Card variant="outlined">
       <Typography variant="h6">{title}</Typography>
@@ -13,8 +21,10 @@ export default function TopList({ data, title, statKey }) {
               <Typography component="span">
                 {`${index + 1}. `}
               </Typography>
-              <Typography component="span" sx={{ textDecoration: 'underline' }}>
-                {item.name}
+              <Typography component="span" sx={{ 
+                textDecoration: 'underline',
+                }}>
+                {truncatePlayerName(item.name)}
               </Typography>
               <Typography component="span">
                 {` - ${item[statKey]}`}
