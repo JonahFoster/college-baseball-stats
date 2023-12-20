2898fa5c5542ae8f0b591e5b461a41fed2e6bdee
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-12-20 11:19:48 -0500

    webpack-config changed to target web

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  target: 'node', 
-  entry: './node.mjs', 
+  target: 'web', // Changed from 'node' to 'web'
+  entry: './node.mjs',
   output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js', 
-    libraryTarget: 'commonjs2' 
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // If you need a library target, choose one appropriate for the web, or you might not need this line at all.
   },
   externals: {
-    'mysql2': 'commonjs2 mysql2' 
+    'mysql2': 'commonjs2 mysql2' // Review if this is needed for the web target
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/, 
-        exclude: /node_modules/, 
+        test: /\.m?js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', 
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'] 
+            presets: ['@babel/preset-env']
           }
         }
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.mjs'], 
-    fullySpecified: false 
+    extensions: ['.js', '.mjs'],
+    fullySpecified: false
   },
   optimization: {
-    minimize: false 
-  },
-  devtool: 'source-map' 
-};
+    minimize
